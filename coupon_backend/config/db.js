84a06467729b4bfc8eaa4fd429857cdb4f2a3237
KetanHegde <ketanhegde2003@gmail.com>
2025-03-15 23:12:59 +0530

    Update db.js

--- coupon_backend/config/db.js
+++ coupon_backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/couponDB", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
