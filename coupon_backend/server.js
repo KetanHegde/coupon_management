576cf295836c4de92ff805f7151fa605b751c08b
KetanHegde <ketanhegde2003@gmail.com>
2025-03-16 09:57:39 +0530

    Update server.js

--- coupon_backend/server.js
+++ coupon_backend/server.js
@@ -12,6 +12,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.set("trust proxy", true);
+
 app.use(cookieParser());
 
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
